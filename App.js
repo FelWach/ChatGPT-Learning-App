0e19e1a4133d5415a79d0b77068f58137134f796
LauraMariaPuehringer <s2110238057@students.fh-hagenberg.at>
2023-11-03 14:47:26 +0100

    .env Variables import

--- App.js
+++ App.js
@@ -2,7 +2,9 @@ import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import BackendSend from "./components/BackendSend";
 
+
 export default function App() {
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
--- components/BackendSend.js
+++ components/BackendSend.js
@@ -1,13 +1,17 @@
 import { View, Text, TextInput, Button } from "react-native";
 import React, { useState } from "react";
 import axios from "axios";
+import {OPENAI_API_KEY, IP_ADDRESS, PORT} from '@env'
 
 export default function BackendSend() {
+
   const [input, setInput] = useState("");
+  //const ipAddress = process.env.EXPO_PUBLIC_IP_ADDRESS;
+  //const port = process.env.EXPO_PUBLIC_PORT;
 
   const sendData = () => {
     axios
-      .post("https://pro5-backend.onrender.com/saveData", {
+      .post("http://" + IP_ADDRESS + ":" + PORT + "/saveData", {
         data: input,
       })
       .then((response) => {
