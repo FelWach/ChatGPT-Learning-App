73b2f39a39b45e409d262f0089bc97338a817a8e
Barbara <huem.barbara@gmail.com>
2023-11-07 15:36:15 +0200

    card handler modified

--- screens/Learning.tsx
+++ screens/Learning.tsx
@@ -1,7 +1,6 @@
 import { useEffect, useState } from 'react';
 import { Text, View, StyleSheet } from 'react-native';
-import { Progress, SizeTokens, YStack, Card} from 'tamagui'
-import axios from 'axios';
+import { Progress, SizeTokens, YStack, Card, Button} from 'tamagui';
 
 export default function Learning() {
 
@@ -15,10 +14,15 @@ export default function Learning() {
       ID: 2,
       Q: "What is the capital of Austria?",
       A: "Vienna"
+    },
+    {
+      ID: 3,
+      Q: "What is the capital of Germany?",
+      A: "Berlin"
     }
   ]);
   const [size, setSize] = useState(4)
-  const [progress, setProgress] = useState(20)
+  const [progress, setProgress] = useState(0)
   const sizeProp = `$${size}` as SizeTokens
 
   const [currID, setCurrID]= useState(data[0].ID);
@@ -26,21 +30,30 @@ export default function Learning() {
   const [currA, setCurrA] = useState(data[0].A);
 
   const [isFront, setIsFront] = useState(true);
-
+  const [isFinished, setIsFinished] = useState(false);
 
   useEffect(() => {
-    const timer = setTimeout(() => setProgress(0), 100)
-    return () => {
-      clearTimeout(timer)
-    }
+      /*async function fetchData() {
+        const response = await fetch('http://localhost:3000/main');
+        const data = await response.json();
+        setData(data);
+      }
+      fetchData();*/
   }, [])
 
-  const handleQuestion = () => {
+  const handleQuestions = () => {
+    setProgress((prev) => (prev + 100/data.length));
+
+    if (currID === data.length) {
+      setIsFront(true);
+      setIsFinished(true);
+      return;
+    }
+
     if (!isFront) {
       setIsFront(true);
     }
-    setProgress((prev) => (prev + 100/data.length) % 100);
-
+    
     if (currID < data.length) {
       setCurrID(currID + 1);
       setCurrQ(data[currID].Q);
@@ -49,38 +62,71 @@ export default function Learning() {
   }
 
   const handleCard = () => {
-    setIsFront(!isFront);
+    if (isFinished) {
+      return;
+    }
+    if (isFinished && !isFront) {
+      return;
+    }
+    setIsFront(false);
+  }
+
+  const repeatOneQuestion = () => {
+    if (isFront) {
+      return;
+    }
+    setIsFront(true);
+  }
+
+  const repeatAllQuestions = () => {
+    setCurrID(1);
+    setCurrQ(data[0].Q);
+    setCurrA(data[0].A);
+    setProgress(0);
+    setIsFront(true);
+    setIsFinished(false);
   }
     
   return (
   <View>
     <Text>Frage {currID} von {data.length}</Text>
-      <Text onPress={() => handleQuestion()}>Richtig</Text>
-      <Text>Nochmal lernen</Text>
       <View>
+        <Button>
+          X
+        </Button>
+
         <View>
+          {!isFront ?
+              <View>
+                <Text onPress={() => handleQuestions()}>Richtig</Text>
+                <Text onPress={() => repeatOneQuestion()}>Falsch</Text>
+              </View> :
+              isFinished &&
+              <Text onPress={() => repeatAllQuestions()}>Fragen wiederholen</Text> 
+          }
           <Card elevate size="$20" borderRadius="$10" onPress={() => handleCard()}>
             <Card.Header padded>
             </Card.Header>
             <Card.Footer padded>
 
             </Card.Footer>
-            <Card.Background>
+            <Card.Background alignItems="center">
               <Text style={styles.cardText}>
-                {isFront ? currQ : currA}
+                {isFinished ? "You're done!" : 
+                  isFront ? currQ : currA
+                }
               </Text>
             </Card.Background>
           </Card>
         </View>
       </View>
-
+    <Text>Swipe to complete the question</Text>
+    <Text>Tap the Card to reveal the answer</Text>
     <YStack height={60} alignItems="center" space>
       <Progress size={sizeProp} marginTop="$5" value={progress}>
         <Progress.Indicator animation="bouncy" />
       </Progress>
     </YStack>
-    <Text>Swipe to complete the question</Text>
-    <Text>Tap the Card to reveal the answer</Text>
 
   </View>
   );
@@ -91,6 +137,5 @@ const styles = StyleSheet.create({
     fontSize: 20,
     fontWeight: "bold",
     textAlign: "center",
-    marginTop: 180,
-  }
+  },
 })
