4bbed05b4d4421368ed8d56de90b8309540dbda9
miriampalmetshofer <miriam.palmetshofer@students.fh-hagenberg.at>
2023-11-09 09:58:14 +0100

    Added delete functionality

--- components/DocumentSelect.tsx
+++ components/DocumentSelect.tsx
@@ -1,11 +1,25 @@
-import { Upload } from '@tamagui/lucide-icons';
+import { Upload, X } from '@tamagui/lucide-icons';
 import * as DocumentPicker from 'expo-document-picker';
 import { useState } from 'react';
-import { Button, Text, View } from 'tamagui';
+import { Button, Text, View, XStack } from 'tamagui';
+
+interface PDFFile {
+    uri: string;
+    name: string;
+    type: string | undefined;
+    size: number | undefined;
+}
 
 export function DocumentSelect() {
-    const [uploaded, setUploaded] = useState("false");
-    const [formData, setFormData] = useState(new FormData());
+    const [files, setFiles] = useState<PDFFile[]>([]);
+
+    const deleteFile = (index: number) => () => {
+        const newFiles = [...files];
+        newFiles.splice(index, 1);
+        setFiles(newFiles);
+        console.log(newFiles);
+    }
+
     const pickPdf = async () => {
         try {
             const docRes = await DocumentPicker.getDocumentAsync({
@@ -16,10 +30,15 @@ export function DocumentSelect() {
             if (!assets) return;
 
             const file = assets[0];
-            const pdfFile: Blob = new Blob([file.uri], { type: file.mimeType });
+            const pdfFile: PDFFile = {
+                uri: file.uri,
+                name: file.name,
+                type: file.mimeType,
+                size: file.size,
+            };
 
-            formData.append("pdfFile", pdfFile);
-            setUploaded("true");
+            setFiles([...files, pdfFile]);
+            console.log(pdfFile);
 
         } catch (err) {
             console.log(err);
@@ -27,9 +46,17 @@ export function DocumentSelect() {
     }
 
     return (
-        <View>
+        <View space="$3" marginBottom="$3">
             <Button onPress={pickPdf} icon={<Upload size={20} />} size="$5" variant='outlined' height="$7">Pick a PDF</Button>
-            <Text>{uploaded}</Text>
+            {files.map((file, index) => {
+                return (
+                    <XStack key={index} alignItems='center' justifyContent='space-between' space>
+                        <Text width={250}>{file.name}</Text>
+                        <Button icon={X} width="$3" height="$3" onPress={deleteFile(index)} />
+                    </XStack>
+                )
+            }
+            )}
         </View>
     )
 }
