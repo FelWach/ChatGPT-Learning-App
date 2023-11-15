214058c8ee6c37804e9f2a7ff1c82717c4975ac1
LauraMariaPuehringer <s2110238057@students.fh-hagenberg.at>
2023-11-15 15:26:54 +0100

    fixed 'App.tsx' import

--- Routes.tsx
+++ Routes.tsx
@@ -1,5 +1,5 @@
 // Stack Component
-import { Stack } from '/App'
+import { Stack } from './App'
 
 // import Screens
 import StartScreen from './screens/StartScreen'
@@ -9,14 +9,14 @@ import Register from './screens/Register'
 export default function Routes() {
 
     const screens: Stack[] = [
-        <Stack.Screen name="StartScreen" component={StartScreen} options={{ headerShown: false }} key={name} />,
-        <Stack.Screen name="Login" component={Login} options={{ title: "" }} key={name} />,
-        <Stack.Screen name="Register" component={Register} options={{ title: "" }} key={name} />,
-        <Stack.Screen name="LearnSet" component={StartScreen} options={{ headerShown: false }} key={name} />,
-        <Stack.Screen name="Profile" component={StartScreen} options={{ headerShown: false }} key={name} />,
-        <Stack.Screen name="Configurator" component={StartScreen} options={{ title: "" }} key={name} />,
-        <Stack.Screen name="QuestionsCatalogue" component={StartScreen} options={{ title: "" }} key={name} />,
-        <Stack.Screen name="LearnMode" component={StartScreen} options={{ title: "" }} key={name} />
+        <Stack.Screen name="StartScreen" component={StartScreen} options={{ headerShown: false }}  />,
+        <Stack.Screen name="Login" component={Login} options={{ title: "" }}  />,
+        <Stack.Screen name="Register" component={Register} options={{ title: "" }}  />,
+        <Stack.Screen name="LearnSet" component={StartScreen} options={{ headerShown: false }}  />,
+        <Stack.Screen name="Profile" component={StartScreen} options={{ headerShown: false }}  />,
+        <Stack.Screen name="Configurator" component={StartScreen} options={{ title: "" }}  />,
+        <Stack.Screen name="QuestionsCatalogue" component={StartScreen} options={{ title: "" }}  />,
+        <Stack.Screen name="LearnMode" component={StartScreen} options={{ title: "" }}  />
        ];
 
     return (
