eca490e434f7befc767be6bd4ca50ffb8b31d761
LauraMariaPuehringer <s2110238057@students.fh-hagenberg.at>
2023-11-16 16:43:03 +0100

    added API Endpoint calls

--- api/api.ts
+++ api/api.ts
@@ -1,5 +1,4 @@
 import axios from 'axios'
-
 // .env variables
 const openaiApiKey = process.env.EXPO_PUBLIC_OPENAI_API_KEY;
 const localhost = process.env.EXPO_PUBLIC_IP_ADDRESS;
@@ -11,7 +10,7 @@ import { UserProps } from "./type"
 // base URL
 const baseUrl = `http://${localhost}:${port}`
 
-// post addUser
+// post addUser: for registration process
 export async function addUser(data: UserProps) {
     try{
         const response = await axios.post(`${baseUrl}/addUser`,  data, {headers: { 'Content-Type': 'application/json'}});
@@ -22,8 +21,85 @@ export async function addUser(data: UserProps) {
     }
 }
 
-// get entries
+// post generate: generates Q&As and saves them in the database (currently only for 'user 1'), needs topic in request body
+export async function generate(data: UserProps) {
+    try{
+        const response = await axios.post(`${baseUrl}/addUser`,  data, {headers: { 'Content-Type': 'application/json'}});
+        return response
+    }
+    catch (error) {
+        console.log('Error: ' + error)
+    }
+}
+
+// post generate: generates Q&As and saves them in the database (currently only for 'user 1'), topic as url param
+export async function generate(topic: string) {
+    try{
+        const response = await axios.post(`${baseUrl}/addUser`,  data, {headers: { 'Content-Type': 'application/json'}});
+        return response
+    }
+    catch (error) {
+        console.log('Error: ' + error)
+    }
+}
+
+// post setConfigurations: saves the configurator settings
+export async function setConfigurations(settings: ) {
+    try{
+        const response = await axios.get(`${baseUrl}/entries`, {headers: { 'Content-Type': 'application/json'}});
+        return response
+    }
+    catch (error) {
+        console.log('Error: ' + error)
+    }
+}
+
+// get entries: returns all Q&As with id and topic
+export async function entries() {
+    try{
+        const response = await axios.get(`${baseUrl}/entries`, {headers: { 'Content-Type': 'application/json'}});
+        return response
+    }
+    catch (error) {
+        console.log('Error: ' + error)
+    }
+}
+
+// get entries: returns all Q&As from a user
 export async function entries(id: number) {
+    try{
+        const response = await axios.get(`${baseUrl}/entries`, {params: {id: id}, {headers: { 'Content-Type': 'application/json'}});
+        return response
+    }
+    catch (error) {
+        console.log('Error: ' + error)
+    }
+}
+
+// get entries: returns all Q&As from a user for a specific topic
+export async function entries(id: number, topic: string) {
+    try{
+        const response = await axios.get(`${baseUrl}/entries`, {params: {userId: id}, {headers: { 'Content-Type': 'application/json'}});
+        return response
+    }
+    catch (error) {
+        console.log('Error: ' + error)
+    }
+}
+
+// get entry: returns a specific Q&A
+export async function entry(id: number) {
+    try{
+        const response = await axios.get(`${baseUrl}/entries`, {params: {userId: id}, {headers: { 'Content-Type': 'application/json'}});
+        return response
+    }
+    catch (error) {
+        console.log('Error: ' + error)
+    }
+}
+
+// delete entry: deletes a specific Q&A
+export async function deleteEntry(id: number) {
     try{
         const response = await axios.get(`${baseUrl}/entries`, {params: {userId: id}, {headers: { 'Content-Type': 'application/json'}});
         return response
