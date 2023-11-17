e5e45b281aa14ba905068acbf9d11d41c9508c85
LauraMariaPuehringer <s2110238057@students.fh-hagenberg.at>
2023-11-17 15:13:32 +0100

    Profile Screen

--- screens/Profile.tsx
+++ screens/Profile.tsx
@@ -1,4 +1,4 @@
-import { Button, H2, Text, Input, SizableText, Tabs, Trigger, View, XStack, YStack } from 'tamagui';
+import { Button, H2, Text, Input, SizableText, Tabs, Trigger, View, XStack } from 'tamagui';
 import { User,  BookOpen } from '@tamagui/lucide-icons'
 import { useAtom  } from 'jotai'
 import { emailAtom, passwordAtom } from '../state/atoms'
@@ -11,32 +11,36 @@ export default function Profile({ navigation }){
 
   return (
     <View>
+        <H2>Your Profile</H2>
 
-      <H2>Your Profile</H2>
+        <XStack flexDirection='column' margin='$8' space>
+          <Input
+            value={email}
+            onChangeText={setEmail}
+            placeholder={'Email'}
+          />
+          <Input
+            value={password}
+            onChangeText={setPassword}
+            placeholder={'Password'}
+            secureTextEntry={true}
+          />
+          <Button borderColor='black' onPress={() => navigation.navigate('StartScreen')}>Sign out</Button>
+          </XStack>
 
-      <Input
-        value={email}
-        onChangeText={setEmail}
-        placeholder={'Email'}
-      />
-      <Input
-        value={password}
-        onChangeText={setPassword}
-        placeholder={'Password'}
-        secureTextEntry={true}
-      />
-      <Button onPress={() => navigation.navigate('StartScreen')}>Sign out</Button>
 
-      <Tabs value='topicsOverview'  flexDirection='row' justifyContent='center'>
-         <Tabs.List>
-          <Tabs.Tab value="profile">
-              <Button unstyled={true} icon={<User size='$2' />} onPress={  () => navigation.navigate('Profile') } />
-          </Tabs.Tab>
-          <Tabs.Tab value="topicsOverview">
-              <Button unstyled={true} icon={<BookOpen size='$2' />} onPress={ () => navigation.navigate('TopicsOverview') } />
-          </Tabs.Tab>
-         </Tabs.List>
-      </Tabs>
+        <XStack  flexDirection='column' alignItems='center' padding='$4'>
+          <Tabs value='topicsOverview'  >
+             <Tabs.List>
+              <Tabs.Tab value="profile">
+                  <Button unstyled={true} justifyContent='center' size='$4' icon={<User size='$2'/>} onPress={  () => navigation.navigate('Profile') } />
+              </Tabs.Tab>
+              <Tabs.Tab value="topicsOverview">
+                  <Button unstyled={true} justifyContent='center' size='$4' icon={<BookOpen size='$2'/>} onPress={ () => navigation.navigate('TopicsOverview') } />
+              </Tabs.Tab>
+             </Tabs.List>
+          </Tabs>
+          </XStack>
     </View>
   );
 };
--- screens/TopicsOverview.tsx
+++ screens/TopicsOverview.tsx
@@ -1,4 +1,4 @@
-import { Button, H2, Text, Input, View, SizableText, Tabs, Trigger } from 'tamagui';
+import { Button, H2, Text, Input, View, SizableText, Tabs, Trigger, XStack } from 'tamagui';
 import { User,  BookOpen } from '@tamagui/lucide-icons'
 
 export default function TopicsOverview({ navigation }){
@@ -6,16 +6,18 @@ export default function TopicsOverview({ navigation }){
     return(
     <View>
         <H2>Your Learn Sets</H2>
-        <Tabs value='profile' flexDirection='row' justifyContent='center'>
-           <Tabs.List>
-              <Tabs.Tab value="profile">
-                   <Button unstyled={true} icon={<User size='$2' />} onPress={  () => navigation.navigate('Profile') }/>
-              </Tabs.Tab>
-              <Tabs.Tab value="topicsOverview">
-                    <Button unstyled={true} icon={<BookOpen size='$2' />} onPress={ () => navigation.navigate('TopicsOverview') } />
-              </Tabs.Tab>
-           </Tabs.List>
-        </Tabs>
+        <XStack  flexDirection='column' alignItems='center' padding='$4'>
+            <Tabs value='profile'>
+               <Tabs.List>
+                  <Tabs.Tab value="profile" >
+                       <Button unstyled={true}  justifyContent='center' size='$4' icon={<User size='$2'/>} onPress={  () => navigation.navigate('Profile') }/>
+                  </Tabs.Tab>
+                  <Tabs.Tab value="topicsOverview" >
+                        <Button unstyled={true} justifyContent='center' size='$4' icon={<BookOpen size='$2'/>} onPress={ () => navigation.navigate('TopicsOverview') } />
+                  </Tabs.Tab>
+               </Tabs.List>
+            </Tabs>
+        </XStack>
     </View>
     )
 }
