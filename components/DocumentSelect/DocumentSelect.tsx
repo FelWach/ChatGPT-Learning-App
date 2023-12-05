c3e75165fd74d005c0baca627f24e47acfce311d
FelWach <s2110238071@fhooe.at>
2023-12-05 11:42:17 +0100

    PDF Upload
    PDF Upload should work now
    
    //commented out old code

--- components/DocumentSelect/DocumentSelect.tsx
+++ components/DocumentSelect/DocumentSelect.tsx
@@ -1,68 +1,133 @@
-import { Upload, X } from '@tamagui/lucide-icons';
-import * as DocumentPicker from 'expo-document-picker';
-import { Button, Spinner, Text, View, XStack } from 'tamagui';
+import { Upload, X } from "@tamagui/lucide-icons";
+import * as DocumentPicker from "expo-document-picker";
+import { Button, Spinner, Text, View, XStack } from "tamagui";
 import { atom, useAtom } from "jotai";
-import { filesArray } from './atoms';
-import { PDFFile } from './types';
+import { filesArray } from "./atoms";
+import { PDFFile } from "./types";
+
+import axios from "axios";
 
 const showSpinner = atom(false);
 
 export function DocumentSelect() {
-    const [files, setFiles] = useAtom(filesArray);
-    const [spinner, setSpinner] = useAtom(showSpinner);
+  const [files, setFiles] = useAtom(filesArray);
+  const [spinner, setSpinner] = useAtom(showSpinner);
 
-    const deleteFile = (index: number) => () => {
-        const newFiles = [...files];
-        newFiles.splice(index, 1);
-        setFiles(newFiles);
-        console.log(newFiles);
-    }
+  const deleteFile = (index: number) => () => {
+    const newFiles = [...files];
+    newFiles.splice(index, 1);
+    setFiles(newFiles);
+    console.log(newFiles);
+  };
 
-    const pickPdf = async () => {
-        try {
-            setSpinner(true);
-            const docRes = await DocumentPicker.getDocumentAsync({
-                type:
-                    "application/pdf"
-            });
-            setSpinner(false);
-            const assets = docRes.assets;
-            if (!assets) return;
+  // const pickPdf = async () => {
+  //     try {
+  //         setSpinner(true);
+  //         const docRes = await DocumentPicker.getDocumentAsync({
+  //             type:
+  //                 "application/pdf"
+  //         });
+  //         setSpinner(false);
+  //         const assets = docRes.assets;
+  //         if (!assets) return;
 
-            const file = assets[0];
-            const pdfFile: PDFFile = {
-                uri: file.uri,
-                name: file.name,
-                type: file.mimeType,
-                size: file.size,
-            };
+  //         const file = assets[0];
+  //         const pdfFile: PDFFile = {
+  //             uri: file.uri,
+  //             name: file.name,
+  //             type: file.mimeType,
+  //             size: file.size,
+  //         };
 
-            setFiles([...files, pdfFile]);
-            console.log(pdfFile);
+  //         setFiles([...files, pdfFile]);
+  //         console.log(pdfFile);
 
-        } catch (err) {
-            console.log(err);
+  //     } catch (err) {
+  //         console.log(err);
+  //     }
+  // }
+
+  const pickPdf = async () => {
+    try {
+      const docRes = await DocumentPicker.getDocumentAsync({
+        type: "application/pdf",
+      });
+
+      const formData = new FormData();
+      const assets = docRes.assets;
+      if (!assets) return;
+
+      const file = assets[0];
+
+      const pdfFile = {
+        name: file.name.split(".")[0],
+        uri: file.uri,
+        type: file.mimeType,
+        size: file.size,
+      };
+
+      formData.append("pdfFile", pdfFile as any);
+
+      const { data } = await axios.post(
+        "http://10.0.2.2:3000/upload",
+        formData,
+        {
+          headers: {
+            Accept: "application/json",
+            "Content-Type": "multipart/form-data",
+          },
         }
+      );
+      console.log(data);
+    } catch (error) {
+      console.log("Error while selecting file: ", error);
     }
+  };
 
-    return (
-        <View space="$3" marginBottom="$3">
-            {files.length < 1 ? (
-                <Button onPress={pickPdf} icon={<Upload size={20} />} size="$5" variant='outlined' height="$7">Pick a PDF</Button>
-            ) : (
-                <Button onPress={pickPdf} icon={<Upload size={20} />} size="$3" variant='outlined' height="$5">Add More</Button>
-            )}
-            {files.map((file, index) => {
-                return (
-                    <XStack key={index} alignItems='center' justifyContent='space-between' space>
-                        <Text width={250} fontSize="$5">{file.name}</Text>
-                        <Button icon={X} width="$3" height="$3" onPress={deleteFile(index)} />
-                    </XStack>
-                )
-            }
-            )}
-            {spinner && <Spinner size="large" color="$blue10" />}
-        </View>
-    )
+  return (
+    <View space="$3" marginBottom="$3">
+      {files.length < 1 ? (
+        <Button
+          onPress={pickPdf}
+          icon={<Upload size={20} />}
+          size="$5"
+          variant="outlined"
+          height="$7"
+        >
+          Pick a PDF
+        </Button>
+      ) : (
+        <Button
+          onPress={pickPdf}
+          icon={<Upload size={20} />}
+          size="$3"
+          variant="outlined"
+          height="$5"
+        >
+          Add More
+        </Button>
+      )}
+      {files.map((file, index) => {
+        return (
+          <XStack
+            key={index}
+            alignItems="center"
+            justifyContent="space-between"
+            space
+          >
+            <Text width={250} fontSize="$5">
+              {file.name}
+            </Text>
+            <Button
+              icon={X}
+              width="$3"
+              height="$3"
+              onPress={deleteFile(index)}
+            />
+          </XStack>
+        );
+      })}
+      {spinner && <Spinner size="large" color="$blue10" />}
+    </View>
+  );
 }
-
--- screens/Configurator/Configurator.tsx
+++ screens/Configurator/Configurator.tsx
@@ -1,166 +1,260 @@
-import { Label, Slider, Button, Text, ScrollView, YStack, XStack, Input, ToggleGroup } from "tamagui"
+import {
+  Label,
+  Slider,
+  Button,
+  Text,
+  ScrollView,
+  YStack,
+  XStack,
+  Input,
+  ToggleGroup,
+} from "tamagui";
 import { DropdownMenu } from "../../components/DropdownMenu/DropdownMenu";
 import { DocumentSelect } from "../../components/DocumentSelect/DocumentSelect";
 import { atom, useAtom } from "jotai";
 import { filesArray } from "../../components/DocumentSelect/atoms";
-import { creativityAtom, languageAtom, languageStyleAtom, questionAtom, dropdownMenuLanguageAtom, dropdownMenuLanguageStyleAtom, dropdownMenuQuestionAtom, difficultyAtom, dropdownMenuDifficultyAtom, topicAtom } from "./atoms";
-import { Lock, Brush } from '@tamagui/lucide-icons'
+import {
+  creativityAtom,
+  languageAtom,
+  languageStyleAtom,
+  questionAtom,
+  dropdownMenuLanguageAtom,
+  dropdownMenuLanguageStyleAtom,
+  dropdownMenuQuestionAtom,
+  difficultyAtom,
+  dropdownMenuDifficultyAtom,
+  topicAtom,
+} from "./atoms";
+import { Lock, Brush } from "@tamagui/lucide-icons";
 import { SaveAreaView } from "../../components/SafeAreaView";
-import { GenerateProps, ConfigSettingsProps, UploadProps } from "../../api/types";
+import {
+  GenerateProps,
+  ConfigSettingsProps,
+  UploadProps,
+} from "../../api/types";
 import { generate, setConfiguration, upload } from "../../api/api";
 
 const selectedValueAtom = atom("Topic");
 
 export function Configurator() {
+  const [files, setFiles] = useAtom(filesArray);
 
-    const [files, setFiles] = useAtom(filesArray);
+  const [question, setQuestion] = useAtom(questionAtom);
+  const [language, setLanguage] = useAtom(languageAtom);
+  const [languageStyle, setLanguageStyle] = useAtom(languageStyleAtom);
+  const [creativity, setCreativity] = useAtom(creativityAtom);
+  const [difficulty, setDifficulty] = useAtom(difficultyAtom);
+  const [topic, setTopic] = useAtom(topicAtom);
 
-    const [question, setQuestion] = useAtom(questionAtom);
-    const [language, setLanguage] = useAtom(languageAtom);
-    const [languageStyle, setLanguageStyle] = useAtom(languageStyleAtom);
-    const [creativity, setCreativity] = useAtom(creativityAtom);
-    const [difficulty, setDifficulty] = useAtom(difficultyAtom);
-    const [topic, setTopic] = useAtom(topicAtom);
+  const [selectedValue, setSelectedValue] = useAtom(selectedValueAtom);
 
-    const [selectedValue, setSelectedValue] = useAtom(selectedValueAtom);
-
-    const configureSettings = async () => {
-        const config: ConfigSettingsProps = {
-            language: language,
-            languageLevel: languageStyle,
-            temperature: creativity,
-            difficulty: difficulty
-        };
-        console.log(config);
-
-        const response = await setConfiguration(config);
-        if (!response) console.log("No response, could not configure");
-        return response?.data;
+  const configureSettings = async () => {
+    const config: ConfigSettingsProps = {
+      language: language,
+      languageLevel: languageStyle,
+      temperature: creativity,
+      difficulty: difficulty,
     };
+    console.log(config);
 
-    const generateFromTopic = async () => {
-        const generateConfig: GenerateProps = {
-            topic: topic,
-            nbQuestions: Number(question)
-        };
-        console.log(generateConfig);
+    const response = await setConfiguration(config);
+    if (!response) console.log("No response, could not configure");
+    return response?.data;
+  };
 
-        const response = await generate(generateConfig);
-        if (!response) console.log("No response, could not generate");
-        return response?.data;
+  const generateFromTopic = async () => {
+    const generateConfig: GenerateProps = {
+      topic: topic,
+      nbQuestions: Number(question),
     };
+    console.log(generateConfig);
 
-    const generateFromPDF = async () => {
-        console.log("Generate from PDF");
-        
-        const generateConfig: UploadProps = {
-            uri: files[0].uri,
-            name: files[0].name,
-            size: Number(files[0].size),
-        };
-        console.log(generateConfig);
+    const response = await generate(generateConfig);
+    if (!response) console.log("No response, could not generate");
+    return response?.data;
+  };
 
-        const response = await upload(generateConfig);
-        // need to add generate after upload works
-        if (!response) console.log("No response from upload call");
-        return response?.data;
-    };
+  // const generateFromPDF = async () => {
+  //     console.log("Generate from PDF");
 
-    const configureAndGenerate = async () => {
-        //const response = await configureSettings();
-        //console.log("Response: " + response);
+  //     const generateConfig: UploadProps = {
+  //         uri: files[0].uri,
+  //         name: files[0].name,
+  //         size: Number(files[0].size),
+  //     };
+  //     console.log(generateConfig);
 
-        if (selectedValue === "Topic") {
-            const response = await generateFromTopic();
-            console.log("Response: " + response);
-        } else {
-            const response = await generateFromPDF();
-            console.log("Response: " + response);
-        }
-    };
+  //     const response = await upload(generateConfig);
+  //     // need to add generate after upload works
+  //     if (!response) console.log("No response from upload call");
+  //     return response?.data;
+  // };
 
-    return (
-        <ScrollView>
-            <SaveAreaView>
+  // const generateFromPDF = async () => {
+  //     try {
+  //       const docRes = await DocumentPicker.getDocumentAsync({
+  //         type: "audio/*",
+  //       });
 
-                <XStack display="flex" justifyContent="center">
-                    <ToggleGroup type="single" value={selectedValue} onValueChange={setSelectedValue} >
-                        <ToggleGroup.Item value="Topic">
-                            <Text>Choose Topic</Text>
-                        </ToggleGroup.Item>
-                        <ToggleGroup.Item value="Files">
-                            <Text>Upload PDF</Text>
-                        </ToggleGroup.Item>
-                    </ToggleGroup>
-                </XStack>
-                <YStack paddingTop={30} paddingBottom={20}>
-                    {selectedValue === "Topic" ? (
-                        <>
-                            <Label paddingBottom={10}>Topic</Label>
-                            <Input size="$4" borderWidth={2} placeholder="e.g. Javascript" height={70} onChangeText={setTopic} />
-                        </>
-                    ) : (
-                        <DocumentSelect />
-                    )}
-                </YStack>
-                <ConfiguratorBasis />
-                <Button size="$6" theme="active" marginVertical={30} onPress={configureAndGenerate}>Generate</Button>
+  //       const formData = new FormData();
+  //       const assets = docRes.assets;
+  //       if (!assets) return;
 
+  //       const file = assets[0];
 
-            </SaveAreaView >
-        </ScrollView>
-    )
+  //       const audioFile = {
+  //         name: file.name.split(".")[0],
+  //         uri: file.uri,
+  //         type: file.mimeType,
+  //         size: file.size,
+  //       };
+
+  //       formData.append("audioFile", audioFile as any);
+
+  //       const { data } = await axios.post(apiUrl, formData, {
+  //         headers: {
+  //           Accept: "application/json",
+  //           "Content-Type": "multipart/form-data",
+  //         },
+  //       });
+  //       console.log(data);
+  //     } catch (error) {
+  //       console.log("Error while selecting file: ", error);
+  //     }
+  //   };
+
+  const configureAndGenerate = async () => {
+    //const response = await configureSettings();
+    //console.log("Response: " + response);
+
+    if (selectedValue === "Topic") {
+      const response = await generateFromTopic();
+      console.log("Response: " + response);
+    } else {
+      //const response = await generateFromPDF();
+      //console.log("Response: " + response);
+    }
+  };
+
+  return (
+    <ScrollView>
+      <SaveAreaView>
+        <XStack display="flex" justifyContent="center">
+          <ToggleGroup
+            type="single"
+            value={selectedValue}
+            onValueChange={setSelectedValue}
+          >
+            <ToggleGroup.Item value="Topic">
+              <Text>Choose Topic</Text>
+            </ToggleGroup.Item>
+            <ToggleGroup.Item value="Files">
+              <Text>Upload PDF</Text>
+            </ToggleGroup.Item>
+          </ToggleGroup>
+        </XStack>
+        <YStack paddingTop={30} paddingBottom={20}>
+          {selectedValue === "Topic" ? (
+            <>
+              <Label paddingBottom={10}>Topic</Label>
+              <Input
+                size="$4"
+                borderWidth={2}
+                placeholder="e.g. Javascript"
+                height={70}
+                onChangeText={setTopic}
+              />
+            </>
+          ) : (
+            <DocumentSelect />
+          )}
+        </YStack>
+        <ConfiguratorBasis />
+        <Button
+          size="$6"
+          theme="active"
+          marginVertical={30}
+          onPress={configureAndGenerate}
+        >
+          Generate
+        </Button>
+      </SaveAreaView>
+    </ScrollView>
+  );
 }
 
 function ConfiguratorBasis() {
-    const [questions] = useAtom(dropdownMenuQuestionAtom);
-    const [languages] = useAtom(dropdownMenuLanguageAtom);
-    const [difficulties] = useAtom(dropdownMenuDifficultyAtom);
-    const [languageStyles] = useAtom(dropdownMenuLanguageStyleAtom);
-    const [accurateness, setAccurateness] = useAtom(creativityAtom);
+  const [questions] = useAtom(dropdownMenuQuestionAtom);
+  const [languages] = useAtom(dropdownMenuLanguageAtom);
+  const [difficulties] = useAtom(dropdownMenuDifficultyAtom);
+  const [languageStyles] = useAtom(dropdownMenuLanguageStyleAtom);
+  const [accurateness, setAccurateness] = useAtom(creativityAtom);
 
-    return (
-        <YStack space>
-            <YStack space={10}>
-                <Label>Questions</Label>
-                <DropdownMenu items={questions} label={"Number of Questions"} atom={questionAtom} />
-            </YStack>
-            <YStack space={10}>
-                <Label>Language</Label>
-                <DropdownMenu items={languages} label={"Language"} atom={languageAtom}
-                />
-            </YStack>
-            <YStack space={10}>
-                <Label>Language Style</Label>
-                <DropdownMenu items={languageStyles} label={"Language style"} atom={languageStyleAtom}
-                />
-            </YStack>
-            <YStack space={10}>
-                <Label>Difficulty</Label>
-                <DropdownMenu items={difficulties} label={"Language style"} atom={difficultyAtom}
-                />
-            </YStack>
-            <YStack paddingTop={20}>
-                <Label>Creativity</Label>
-                <YStack paddingTop={30}>
-                    <Slider defaultValue={[50]} max={100} step={1} onValueChange={(value) => setAccurateness(value[0])}>
-                        <Slider.Track>
-                            <Slider.TrackActive />
-                        </Slider.Track>
-                        <Slider.Thumb index={0} circular elevate />
-                    </Slider>
-                </YStack>
-                <XStack paddingTop={20} paddingHorizontal={5} display="flex" justifyContent="space-between">
-                    <YStack space="$2">
-                        <Lock size="$1" />
-                        <Text fontSize="$1">Stick to the topic</Text>
-                    </YStack>
-                    <YStack alignItems="flex-end" space="$2">
-                        <Brush size="$1" />
-                        <Text fontSize="$1">Get creative</Text>
-                    </YStack>
-                </XStack>
-            </YStack>
+  return (
+    <YStack space>
+      <YStack space={10}>
+        <Label>Questions</Label>
+        <DropdownMenu
+          items={questions}
+          label={"Number of Questions"}
+          atom={questionAtom}
+        />
+      </YStack>
+      <YStack space={10}>
+        <Label>Language</Label>
+        <DropdownMenu
+          items={languages}
+          label={"Language"}
+          atom={languageAtom}
+        />
+      </YStack>
+      <YStack space={10}>
+        <Label>Language Style</Label>
+        <DropdownMenu
+          items={languageStyles}
+          label={"Language style"}
+          atom={languageStyleAtom}
+        />
+      </YStack>
+      <YStack space={10}>
+        <Label>Difficulty</Label>
+        <DropdownMenu
+          items={difficulties}
+          label={"Language style"}
+          atom={difficultyAtom}
+        />
+      </YStack>
+      <YStack paddingTop={20}>
+        <Label>Creativity</Label>
+        <YStack paddingTop={30}>
+          <Slider
+            defaultValue={[50]}
+            max={100}
+            step={1}
+            onValueChange={(value) => setAccurateness(value[0])}
+          >
+            <Slider.Track>
+              <Slider.TrackActive />
+            </Slider.Track>
+            <Slider.Thumb index={0} circular elevate />
+          </Slider>
         </YStack>
-    )
+        <XStack
+          paddingTop={20}
+          paddingHorizontal={5}
+          display="flex"
+          justifyContent="space-between"
+        >
+          <YStack space="$2">
+            <Lock size="$1" />
+            <Text fontSize="$1">Stick to the topic</Text>
+          </YStack>
+          <YStack alignItems="flex-end" space="$2">
+            <Brush size="$1" />
+            <Text fontSize="$1">Get creative</Text>
+          </YStack>
+        </XStack>
+      </YStack>
+    </YStack>
+  );
 }
