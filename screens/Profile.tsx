0c004d9d22082106b56f37d9c188f5d8286588c4
LauraMariaPuehringer <s2110238057@students.fh-hagenberg.at>
2023-12-06 20:49:42 +0100

    adapted Profile

--- screens/Profile.tsx
+++ screens/Profile.tsx
@@ -1,38 +1,67 @@
-import { Button, H2, Input, View, XStack } from 'tamagui';
-import { atom, useAtom  } from 'jotai'
+import { Button, H2, Input, View, XStack, YStack } from 'tamagui';
+import { Pencil } from '@tamagui/lucide-icons';
+import { atom, useAtom } from 'jotai'
 import { userAtom } from '../state/atoms'
+import { SaveAreaView } from '../components/SafeAreaView';
 import TabNavigator from '../components/TabNavigator/TabNavigator'
 
 const passwordAtom = atom<string>('');
+const editUsernameAtom = atom<boolean>(false);
+const editEmailAtom = atom<boolean>(false);
+const editPasswordAtom = atom<boolean>(false);
 
-export default function Profile({ navigation }){
-  
+export default function Profile({ navigation }) {
+
   const [user, setUser] = useAtom(userAtom);
   const [password, setPassword] = useAtom(passwordAtom);
+  const [editUsername, setEditUsername] = useAtom(editUsernameAtom);
+  const [editEmail, setEditEmail] = useAtom(editEmailAtom);
+  const [editPassword, setEditPassword] = useAtom(editPasswordAtom)
 
   return (
     <View>
+      <SaveAreaView>
         <H2>Your Profile</H2>
 
-        <XStack flexDirection='column' margin='$8' space>
-          <Input
-            value={user.email}
-            onChangeText={(e) => setUser({...user, email: e})}
-            placeholder={'Email'}
-          />
-          <Input
-            value={password}
-            onChangeText={setPassword}
-            placeholder={'Password'}
-            secureTextEntry={true}
-          />
-          <Button borderColor='black' onPress={() => navigation.navigate('StartScreen')}>Sign out</Button>
+        <YStack margin='$8' space>
+          <XStack justifyContent='space-evenly' space>
+          <Input width='85%'
+              value={user.email}
+              editable={editUsername}
+              onChangeText={(e) => setUser({ ...user, name: value })}
+              placeholder={'Email'}
+            />            
+            <Button width='15%' icon={<Pencil size={18} />} onPress={() => setEditUsername(true)}></Button>
           </XStack>
 
+          <XStack justifyContent='space-evenly' space>
+            <Input width='85%'
+              value={user.email}
+              editable={editEmail}
+              onChangeText={(e) => setUser({ ...user, email: e })}
+              placeholder={'Email'}
+            />
+            <Button  width='15%' icon={<Pencil size={18} />} onPress={() => setEditEmail(true)}></Button>
+          </XStack>
 
-        <XStack  flexDirection='column' alignItems='center' padding='$4'>
-            <TabNavigator navigation={navigation} value={'profile'} />
+          <XStack justifyContent='space-evenly' space>
+            <Input width='85%'
+              value={password}
+              editable={editPassword}
+              onChangeText={setPassword}
+              placeholder={'Password'}
+              secureTextEntry={true}
+            /><Button  width='15%' icon={<Pencil size={18} />} onPress={() => setEditPassword(true)}></Button>
+          </XStack>
+
+          <Button width='100%' marginTop='$10' borderColor='black' onPress={() => navigation.navigate('StartScreen')}>Sign out</Button>
+        </YStack>
+
+
+        <XStack flexDirection='column' alignItems='center' padding='$4'>
+          <TabNavigator navigation={navigation} value={'profile'} />
         </XStack>
+      </SaveAreaView>
     </View>
   );
 };
--- screens/UserSettings.tsx
+++ screens/UserSettings.tsx
@@ -11,12 +11,12 @@ const repeatPasswordAtom = atom<string>('');
 const editingPasswordAtom = atom<boolean>(false);
 
 export default function UserSettings({ navigation }) {
-    
+
     const [user, setUser] = useAtom(userAtom);
     const [password, setPassword] = useAtom(passwordAtom);
     const [repeatPassword, setRepeatPassword] = useAtom(repeatPasswordAtom);
     const [isEditingPassword, setIsEditingPassword] = useAtom(editingPasswordAtom);
-  
+
     const saveUserData = () => {
         // Handle changes
     };
@@ -33,52 +33,51 @@ export default function UserSettings({ navigation }) {
         isEditingPassword ? setIsEditingPassword(false) : setIsEditingPassword(true);
         // Handle changes
     };
-  
+
     return (
-      <View>
-        <H1>Your Profile</H1>
-        <XStack alignItems="center" space="$2">
-            <Input
-            value={user.name}
-            onChangeText={(value) => setUser({...user, name: value})}
-            placeholder={'Username'}
-            />
-            <Button onPress={() => editUsername()}>
-            <Pencil />
-            </Button>
-        </XStack>
-        <XStack alignItems="center" space="$2">
-            <Input
-            value={user.email}
-            onChangeText={(value) => setUser({...user, email: value})}
-            placeholder={'Email'}
-            />
-            <Button onPress={() => editEmail()}>
-            <Pencil />
-            </Button>
-        </XStack>
-        <XStack alignItems="center" space="$2">
-            <Input
-            value={password}
-            onChangeText={(value) => setPassword(value)}
-            placeholder={'Password'}
-            secureTextEntry={true}
-            />
-            <Button onPress={() => editPassword()}>
-            <Pencil />
-            </Button>
-        </XStack>
-        {isEditingPassword && 
+        <View>
+            <H1>Your Profile</H1>
             <XStack alignItems="center" space="$2">
                 <Input
-                value={repeatPassword}
-                onChangeText={(value) => setRepeatPassword(value)}
-                placeholder={'Repeat Password'}
+                    value={user.name}
+                    onChangeText={(value) => setUser({ ...user, name: value })}
+                    placeholder={'Username'}
                 />
+                <Button onPress={() => editUsername()}>
+                    <Pencil />
+                </Button>
             </XStack>
-        }
-        <Button onPress={saveUserData, () => navigation.navigate('StartScreen')}>Save</Button>
-      </View>
+            <XStack alignItems="center" space="$2">
+                <Input
+                    value={user.email}
+                    onChangeText={(value) => setUser({ ...user, email: value })}
+                    placeholder={'Email'}
+                />
+                <Button onPress={() => editEmail()}>
+                    <Pencil />
+                </Button>
+            </XStack>
+            <XStack alignItems="center" space="$2">
+                <Input
+                    value={password}
+                    onChangeText={(value) => setPassword(value)}
+                    placeholder={'Password'}
+                    secureTextEntry={true}
+                />
+                <Button onPress={() => editPassword()}>
+                    <Pencil />
+                </Button>
+            </XStack>
+            {isEditingPassword &&
+                <XStack alignItems="center" space="$2">
+                    <Input
+                        value={repeatPassword}
+                        onChangeText={(value) => setRepeatPassword(value)}
+                        placeholder={'Repeat Password'}
+                    />
+                </XStack>
+            }
+            <Button onPress={saveUserData, () => navigation.navigate('StartScreen')}>Save</Button>
+        </View>
     );
-  }
-  
+}
