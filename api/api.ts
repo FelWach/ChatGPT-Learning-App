93036f96c1716e154eb438c8523ce706f13c91b5
LauraMariaPuehringer <s2110238057@students.fh-hagenberg.at>
2024-01-29 08:47:57 +0100

    fixed import

--- api/api.ts
+++ api/api.ts
@@ -4,7 +4,7 @@ const localhost = process.env.EXPO_PUBLIC_IP_ADDRESS;
 const port = process.env.EXPO_PUBLIC_PORT;
 
 // type interfaces
-import { UserProps, UpdatedUserProps, LoginProps, GenerateProps, GenerateFromDocsProps, ConfigSettingsProps, UploadProps } from "./type"
+import { UserProps, UpdatedUserProps, LoginProps, GenerateProps, GenerateFromDocsProps, ConfigSettingsProps, UploadProps } from "./types"
 
 // base URL
 const baseUrl = `http://${localhost}:${port}`
